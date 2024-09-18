640f57c72c3f0d04d642f53a847c1c0095d3860a
Jamie135 <pbureera@paul-f5Ar3s8.clusters.42paris.fr>
2024-09-18 12:04:28 +0200

    FundMe

--- code/Training/42BerryFundMe.sol
+++ code/Training/42BerryFundMe.sol
@@ -1,4 +1,27 @@
 // SPDX-License-Identifier: MIT
-// License used to publish the contract, in our case it is only used to bypass the warning from some compiler
 
 pragma solidity ^0.8.24;
+
+import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+contract BerryFundMe {
+    
+    uint256 public minBerry = 50;
+
+    function fund() public payable {
+        require(msg.value >= minBerry, "Not enough!");
+    }
+
+    function getPrice() public {
+        // ABI
+        // Address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+    }
+
+    function getVersion() public view returns (uint256) {
+        
+    }
+
+    function getConversionRate() public {
+
+    }
+}
