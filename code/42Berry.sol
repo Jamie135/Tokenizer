bd515b067e5c64df6c13433d15d5566d574e2e01
Jooma <jamie.gworld@hotmail.com>
2024-09-22 22:24:36 +0200

    updated contract

--- code/42Berry.sol
+++ code/42Berry.sol
@@ -6,25 +6,34 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 
+// Defines a new contract called `BerryToken`, which inherits from ERC20, Ownable, and Pausable
 contract BerryToken is ERC20, Ownable, Pausable {
 
+    // Constructor that initializes the token with the name "42Berry" and symbol "42B"
+    // It also sets the initial owner of the contract using the Ownable contract.
     constructor(address initialOwner)
         ERC20("42Berry", "42B")
         Ownable(initialOwner)
     {}
 
+    // Function to mint new tokens, only accessible by the owner of the contract
     function mint(address account, uint256 amount) public onlyOwner {
         _mint(account, amount * 10 ** decimals());
     }
 
+    // Function to burn tokens, only accessible by the owner of the contract
     function burn(address account, uint256 amount) public onlyOwner {
         _burn(account, amount * 10 ** decimals());
     }
 
+    // Function to pause the contract, only accessible by the owner of the contract
+    // Pausing the contract will allow the owner to prevent all transfers and approvals
     function pause() public onlyOwner {
         _pause();
     }
 
+    // Function to unpause the contract, only accessible by the owner of the contract
+    // It allows the owner to restore full functionality of the contract
     function unpause() public onlyOwner {
         _unpause();
     }
