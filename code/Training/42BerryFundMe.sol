678c08ff1007cbbf269c1042642b4d125af47389
Jooma <jamie.gworld@hotmail.com>
2024-09-22 11:24:16 +0200

    updated FundMe

--- code/Training/42BerryFundMe.sol
+++ code/Training/42BerryFundMe.sol
@@ -2,26 +2,48 @@
 
 pragma solidity ^0.8.24;
 
-import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
 
 contract BerryFundMe {
+
+    using PriceConverter for uint256;
     
-    uint256 public minBerry = 50;
+    uint256 public minBerry = 50 * 1e18;
+
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+
+    address public owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
 
     function fund() public payable {
-        require(msg.value >= minBerry, "Not enough!");
+        require(msg.value.getConversionRate() >= minBerry, "Not enough!");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
     }
 
-    function getPrice() public {
-        // ABI
-        // Address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+    function withdraw() public {
+
+        for(uint256 index = 0; index < funders.length; index++) {
+            address funder = funders[index];
+            addressToAmountFunded[funder] = 0;
+        }
+        // reset the array to a blank new one
+        funders = new address[](0);
+
+        payable(msg.sender).transfer(address(this).balance);
+        bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        require(sendSuccess, "Send failed");
+
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
     }
 
-    function getVersion() public view returns (uint256) {
-        
-    }
-
-    function getConversionRate() public {
-
+    modifier onlyOwner {
+        require(msg.sender == owner, "Sender is not owner!");
+        _;
     }
 }
